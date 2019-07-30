a0d752e26b4adcaf15653df09cc91116ab1d3866
Eliton <eliton.santos@amcom.com.br>
2019-07-30 16:18:40 -0300

    rotas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import Header from './components/Header'
 import './styles.css';
-import Main from './pages/main'
+import Routes from './routes';
 
 class App extends Component {
   render(){
     return(
       <div className='App'>
         <Header/>
-        <Main/>
+        <Routes/>
       </div>
     );
   }
